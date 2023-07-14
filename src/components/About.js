d62e097494dec25be318bd70a37df56ef865d28f
carolina-barreiro <carolinabarreiro2504@gmail.com>
2023-07-14 11:57:25 +0100

    estrutura base contact-problemas de posicionamento

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import './About.css';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Image from 'react-bootstrap/Image';
+
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,64 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Image from 'react-bootstrap/Image';
+
+
+export function FormContact() {
+    return (
+        <>
+            <Form>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+            <Button href="#" className="btn-custom font-light GothamBook">Contact me</Button>
+        </>
+    );
+}
 
 function Contact() {
     return (
-        <Container fluid="md">
+        <Container className="fluid">
             <Row className="justify-content-md-center" id="projectsPage">
                 <Col className="align-items-center justify-content-center">
                     <h2 className="font-medium">CONTACT ME</h2>
                     <br />
                 </Col>
             </Row>
-            <Row xs={1} md={2} className="justify-content-md-center g-4">
-                
-            </Row>
+            <Container fluid className="container-fluid no-gutters m-0 custom-container ">
+                <Row className="row no-border custom-row justify-content-start align-items-center">
+                    <Card className="card-contact">
+                        <Card.Body>
+                            <Row>
+                                <Col className="d-flex align-items-center flex-column">
+                                    <div className="left-side">
+                                        <Image src="/images/leafy-houseplant-in-vase.png" className="contact-image" />
+                                        <h3 style={{margin: 0}}> Let's chat!</h3>
+                                       
+                                    </div>
+                                        <h6>Please fill in the form andI'll get back to you as soon as possible.</h6>
+
+                                </Col>
+                                <Col>
+                                    <br></br>
+                                    <FormContact />
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Row>
+            </Container>
         </Container>
     );
 }
